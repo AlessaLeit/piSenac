f938bda552c272d6a1a874098b6b82f8bbca0603
AlessaLeit <alessandra.leite@alunos.sc.senac.br>
2025-05-19 15:59:58 -0300

    CRUD produto

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from queue import Full
 import re
 
 from cliente import Cliente
+from produto import Produto
 
 loop = 1 
 clientes_cadastrados = []
+produtos_cadastrados = []
 while loop == 1:
     print("-" * 10 + "  SOFTWARE  " + "-" * 10)
     print("Escolha uma opção:")
@@ -19,6 +21,7 @@ while loop == 1:
     print("[8] Financeiro")
     menu = input("Digite o número correspondente: ")
     
+    #Cliente
     if menu == "1":
         print("-" * 11 + "  CLIENTE  " + "-" * 11)
         print("Escolha uma opção:")
@@ -26,6 +29,7 @@ while loop == 1:
         print("[2] Listar Cliente")
         print("[3] Remover Cliente")
         print("[4] Atualziar Cliente")
+        print("[5] Voltar")
         submenu = input("Digite o número correspondente: ")
         
         # Adicionar
@@ -76,6 +80,76 @@ while loop == 1:
                     cliente_atualizar.atualizar_dados()
                 else:
                     print("Índice inválido.")
+    
+        # Sair
+        elif submenu == "5":
+            exit
+    
+    #Produto
+    if menu == "2":
+        print("-" * 11 + "  Produtos  " + "-" * 11)
+        print("Escolha uma opção:")
+        print("[1] Adicionar Produto")
+        print("[2] Listar Produtos")
+        print("[3] Remover Produto")
+        print("[4] Atualziar Produto")
+        print("[5] Voltar")
+        submenu = input("Digite o número correspondente: ")
+   
+         # Adicionar
+        if submenu == "1":
+            produto = Produto.criar_por_input()
+            if produto: 
+                produtos_cadastrados.append(produto)
+            else:
+                print("Produto não foi cadastrado devido a dados inválidos.")
+
+        # Listar
+        elif submenu == "2":
+            if not produtos_cadastrados:
+                print("Nenhum produto cadastrado.")
+            else:
+                print("\n--- Lista de Produtos Cadastrados ---")
+                for i, produto in enumerate(produtos_cadastrados, start=1):
+                    if produto is not None:
+                        print(f"\nProduto {i}:")
+                        produto.exibir()
+                    else:
+                        print(f"\nProduto {i} inválido (None).")
+        
+        # Remover  
+        elif submenu == "3":
+            if not produtos_cadastrados:
+                print("Nenhum produto para remover.")
+            else:
+                for i, produto in enumerate(produtos_cadastrados, start=1):
+                    print(f"[{i}] {produto.nome} - ID: {produto.id}")
+                indice = int(input("Digite o número do produto que deseja remover: ")) - 1
+                if 0 <= indice < len(produtos_cadastrados):
+                    removido = produtos_cadastrados.pop(indice)
+                    print(f"Produto '{removido.nome}' removido com sucesso.")
+                else:
+                    print("Índice inválido.")
+        
+        # Atualizar  
+        elif submenu == "4":
+            if not produtos_cadastrados:
+                print("Nenhum produto para atualizar.")
+            else:
+                for i, produto in enumerate(produtos_cadastrados, start=1):
+                    print(f"[{i}] {produto.nome} - ID: {produto.id}")
+                indice = int(input("Digite o número do produto que deseja atualizar: ")) - 1
+                if 0 <= indice < len(produtos_cadastrados):
+                    produto_atualizar = produtos_cadastrados[indice]
+                    produto_atualizar.atualizar_dados()
+                else:
+                    print("Índice inválido.")
+       
+        # Sair
+        elif submenu == "5":
+            exit
+    
+
 
 
                 
---
+++ produto.py
@@ -0,0 +1,47 @@
+class Produto:      
+    def __init__(self, nome, valorVenda, valorCompra, fornecedor, estoque, validade, id):
+        self.nome = nome
+        self.valorVenda = valorVenda
+        self.valorCompra = valorCompra
+        self.fornecedor = fornecedor
+        self.estoque = estoque
+        self.validade = validade
+        self.id = id
+
+    def criar_por_input():
+        nome = input("Nome: ")
+        valorVenda = input("Valor de Venda: ")
+        valorCompra = input("Valor de Compra: ")
+        fornecedor = input("CNPJ Fornecedor:" )
+        estoque = input("Quantidade em estoque: ")
+        validade = input("Data de Validade: ")
+        id = input("Id do Produto: ")
+          
+        print("Produto Cadastado com sucesso!")
+        return (nome, valorVenda, valorCompra, fornecedor, estoque, validade, id)
+           
+    def atualizar_dados(self):
+        
+        print("Digite os novos dados (ou pressione Enter para manter o valor atual):")
+        novo_nome = input(f"Nome atual: {self.nome}\nNovo nome: ") or self.nome
+        novo_valorVenda = input(f"Valor de Venda atual: R${self.valorVenda}\nNovo valor de Venda: ") or self.valorVenda
+        novo_valorCompra = input(f"Valor de Compra atual: {self.valorCompra}\nNovo valor de Compra: ") or self.valorCompra
+        novo_fornecedor = input(f"Fornecedor atual: {self.fornecedor}\nNovo fornecedor: ") or self.fornecedor
+        novo_validade = input(f"Validade atual: {self.validade}\nNova validade: ") or self.validade
+        novo_id = input(f"Id Atual: {self.id}\nNovo Id: ") or self.id
+
+        self.nome = novo_nome
+        self.valorVenda = novo_valorVenda
+        self.valorCompra = novo_valorCompra
+        self.fornecedor = novo_fornecedor
+        self.validade = novo_validade
+        self.id = novo_id
+
+        print("Produto atualizado com sucesso!")
+
+    def exibir(self):
+        print("-" * 30)
+        print(f"Nome: {self.nome} | Valor: R${self.valorVenda:.2f} | Valor Compra: R${self.valorCompra:.2f} | Fornecedor: {self.fornecedor} | Estoque: {self.estoque} | Validade: {self.validade} | Id: {self.id}")
+
+        
+
